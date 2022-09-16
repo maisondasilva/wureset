2d85a39d2207ff829306c58b6a12820a07e06f32
Maison da Silva <maisonmdsgreen@hotmail.com>
2022-09-16 18:08:55 -0300

    Add files via upload

---
+++ WUReset.bat
@@ -0,0 +1,8 @@
+net stop wuauserv
+net stop bits
+cd %windir%
+ren SoftwareDistribution SoftwareDistribution.old
+net stop cryptsvc
+cd %windir%\System32
+ren Catroot Catroot.old
+ren Catroot2 Catroot2.old
